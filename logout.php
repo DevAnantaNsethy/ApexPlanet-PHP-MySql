f410045b0a31413d9ff0df45baf20856c8d90aa7
Ananta Narayan Sethy <147572531+DevAnantaNsethy@users.noreply.github.com>
2025-09-07 01:33:44 +0530

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,6 +1,23 @@
 <?php
 session_start();
+// clear session array
+$_SESSION = [];
+
+// invalidate cookie
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// destroy
 session_destroy();
+
+// regenerate a new session id
+session_start();
+session_regenerate_id(true);
+
 header("Location: login.php");
 exit();
-?>
