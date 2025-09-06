6c6da9db11da8074a0b976d8c78ab83d05f159f6
Ananta Narayan Sethy <147572531+DevAnantaNsethy@users.noreply.github.com>
2025-09-06 22:57:15 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,10 +1,10 @@
 <?php
 $host = "localhost";
-$user = "root";   // default XAMPP MySQL user
-$pass = "";       // default is empty (no password)
-$db   = "blog";
+$user = "root";   // default XAMPP username
+$pass = "";       // default XAMPP password is empty
+$dbname = "testdb";
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
